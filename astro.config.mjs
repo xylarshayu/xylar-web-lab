b7764d13b739ad073d89192c2aa1ac7334c3a74f
xylarshayu <xylar.w@gmail.com>
2023-03-20 05:45:37 +0530

    feat:Icons figured out

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
-import { FontaineTransform } from 'fontaine'
+import { FontaineTransform } from 'fontaine';
+import Icons from 'unplugin-icons/vite';
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,6 +10,9 @@ export default defineConfig({
       FontaineTransform.vite({
         fallbacks: ['Arial'],
         resolvePath: (id) => new URL(`./public${id}`, import.meta.url), // id is the font src value in the CSS
+      }),
+      Icons({
+        compiler: 'astro',
       }),
     ],
   },
