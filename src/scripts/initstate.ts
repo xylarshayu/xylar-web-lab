e14f7a0332b27206df14fc5d7e348e94b85ac295
xylarshayu <xylar.w@gmail.com>
2023-03-26 19:51:23 +0530

    fix: framework toggle bugfix

--- src/scripts/initstate.ts
+++ src/scripts/initstate.ts
@@ -1,3 +1,4 @@
+/* Dark Mode */
 const darkSwitch = ():void => {
   document.documentElement.classList.toggle('dark');
   localStorage?.setItem('isDark', document.documentElement.classList.contains('dark').toString());
@@ -17,15 +18,21 @@ if (setDark()) {
 }
 else document.documentElement.classList.remove('dark');
 
-/* Non Dark mode stuff */
+/* Framework Toggle */
 const frameworks = ['html', 'vue', 'react', 'astro', 'solid', 'svelte'];
-const toggleFramework = (i: string):boolean => document.body.toggleAttribute(i);
-const frameworkActive = (i: string):boolean => document.body.dataset[i] ? true : false;
+const toggleFramework = (i: string):void => {
+  let set = document.body.toggleAttribute(i);
+  localStorage?.setItem(i, set.toString());
+};
+const frameworkActive = (i: string):boolean => document.body.hasAttribute(i);
 
 document.addEventListener('DOMContentLoaded', () => {
   frameworks.map((i) => {
     if (localStorage?.getItem(i) && JSON.parse(localStorage?.getItem(i)!)) toggleFramework(i);
   });
 
-  toggleFramework('html'); toggleFramework('vue'); toggleFramework('astro');
-}); 
+  if (!frameworks.some(frameworkActive)) {toggleFramework('html'); toggleFramework('vue'); toggleFramework('react')};
+
+});
+
+/* */
