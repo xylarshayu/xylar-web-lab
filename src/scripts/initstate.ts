7b61850020823948ea0a4db47b1c887da2fac1cf
xylarshayu <xylar.w@gmail.com>
2023-03-26 20:02:06 +0530

    feat: shifted some initstate code to deferred

--- src/scripts/initstate.ts
+++ src/scripts/initstate.ts
@@ -19,20 +19,5 @@ if (setDark()) {
 else document.documentElement.classList.remove('dark');
 
 /* Framework Toggle */
-const frameworks = ['html', 'vue', 'react', 'astro', 'solid', 'svelte'];
-const toggleFramework = (i: string):void => {
-  let set = document.body.toggleAttribute(i);
-  localStorage?.setItem(i, set.toString());
-};
-const frameworkActive = (i: string):boolean => document.body.hasAttribute(i);
-
-document.addEventListener('DOMContentLoaded', () => {
-  frameworks.map((i) => {
-    if (localStorage?.getItem(i) && JSON.parse(localStorage?.getItem(i)!)) toggleFramework(i);
-  });
-
-  if (!frameworks.some(frameworkActive)) {toggleFramework('html'); toggleFramework('vue'); toggleFramework('react')};
-
-});
 
 /* */
