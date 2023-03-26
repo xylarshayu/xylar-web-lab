75bd07bfa3363bd902935c253f43c8aa9cd269f6
xylarshayu <xylar.w@gmail.com>
2023-03-26 19:25:34 +0530

    feat:added framework toggle with body attributes

--- src/scripts/darkmode.ts
+++ src/scripts/darkmode.ts
@@ -16,3 +16,16 @@ if (setDark()) {
   link!.href = `/assets/xylar_lab_dark.svg`;
 }
 else document.documentElement.classList.remove('dark');
+
+/* Non Dark mode stuff */
+const frameworks = ['html', 'vue', 'react', 'astro', 'solid', 'svelte'];
+const toggleFramework = (i: string):boolean => document.body.toggleAttribute(i);
+const frameworkActive = (i: string):boolean => document.body.dataset[i] ? true : false;
+
+document.addEventListener('DOMContentLoaded', () => {
+  frameworks.map((i) => {
+    if (localStorage?.getItem(i) && JSON.parse(localStorage?.getItem(i)!)) toggleFramework(i);
+  });
+
+  toggleFramework('html'); toggleFramework('vue'); toggleFramework('astro');
+}); 
